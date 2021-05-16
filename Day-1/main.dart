fab2d2310b83de92b892c3374e6273f2bcfca16d
SaicharanSarikonda1410200 <52876325+saicharansarikonda@users.noreply.github.com>
2021-05-16 21:32:33 +0530

    Added little bit styling to main.dart file

--- Day-1/main.dart
+++ Day-1/main.dart
@@ -4,17 +4,45 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar:AppBar(
-        title:Text("my first app"),
-        backgroundColor: Colors.blueGrey,
+        title:Text(
+          "my first app",
+           style: TextStyle(
+             fontFamily: "AnticSlab",
+             fontSize: 20.0,
+           ),
+        ),
+        backgroundColor: Colors.blueGrey[500],
         centerTitle: true,
+        
       ),
       body:Center(
-        child: Text("hey..how you doing"),
+        child: Text(
+          "hey...how you doing",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+            wordSpacing: 2.0,
+            fontFamily: "AnticSlab",
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Text("click"),
+        onPressed: (){},
+        child: Text(
+          "Click",
+          style:TextStyle(
+            color:Colors.green[600],
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5,
+            fontSize: 17.0,
+            fontFamily: "AnticSlab",
+          ),
+        ),
+        backgroundColor: Colors.lime,
       ),
       backgroundColor: Colors.green,
+      
     ),
   ));
 }
