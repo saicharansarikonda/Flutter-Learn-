f8bc5a7f99df9e3b21c6b4d765d1a9de417c05ee
SaicharanSarikonda1410200 <52876325+saicharansarikonda@users.noreply.github.com>
2021-05-17 10:11:29 +0530

    added an icon

---
+++ Day-2/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "my first app",
+          style: TextStyle(
+            fontFamily: "AnticSlab",
+            fontSize: 20.0,
+          ),
+        ),
+        backgroundColor: Colors.blueGrey[500],
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.blueGrey[200],
+          size: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text(
+          "Click",
+          style: TextStyle(
+            color: Colors.green[600],
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5,
+            fontSize: 17.0,
+            fontFamily: "AnticSlab",
+          ),
+        ),
+        backgroundColor: Colors.lime,
+      ),
+      backgroundColor: Colors.green,
+    );
+  }
+}
